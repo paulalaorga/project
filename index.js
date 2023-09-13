0603d44a5920e3b433a2dfbcaf29a6463d7b70dc
paula <paulalaorga@gmail.com>
2023-09-13 21:25:00 +1000

    Initial structure finished

--- index.js
+++ index.js
@@ -15,29 +15,43 @@ async function fetchData(url) {
 }
 
 const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+const clearButton = document.getElementById('clearButton');
+
 const recipeList = document.getElementById('recipeList');
 const selectedRecipeList = document.getElementById('selectedRecipeList');
 const ingredientList = document.getElementById('ingredientList');
 const shoppingList = document.getElementById('shoppingList');
 
-const selectedRecipes = [];
-const shoppingListItems = [];
 
-// Function to display recipes in the recipe list
-function displayRecipes(recipes) {
-    recipeList.innerHTML = ''; 
-    if (recipes) {
-        recipes.forEach((recipe) => {
-            const recipeItem = createRecipeListItem(recipe);
-            recipeList.appendChild(recipeItem);
-        });
-    } else {
-        recipeList.textContent = 'No recipes found.';
+const selectedRecipes = []; // My Menu array
+const shoppingListItems = []; // My Shopping List
+
+// Event listener for the search button and clear button
+searchButton.addEventListener('click',  () => {
+    const query = searchInput.value.trim();
+    if (query !== '') {
+        fetchRecipes(query);
     }
-}
+});
 
-// Function to create a list item for a recipe
-function createRecipeListItem(recipe) {
+searchInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        const query = searchInput.value.trim();
+        if (query !== '') {
+            fetchRecipes(query);
+        }
+    }
+});
+
+ clearButton.addEventListener('click', function() {
+     displayRecipes(), displayIngredientsList() = '';
+ });
+
+
+ // Functions to create and display list of recipes
+
+function createRecipeList(recipe) {
     const recipeItem = document.createElement('li');
     recipeItem.textContent = recipe.strMeal;
     recipeItem.addEventListener('click', () => {
@@ -46,7 +60,20 @@ function createRecipeListItem(recipe) {
     return recipeItem;
 }
 
-// Function to select a recipe
+function displayRecipes(recipes) {
+    recipeList.innerHTML = ''; 
+    if (recipes) {
+        recipes.forEach((recipe) => {
+            const recipeItem = createRecipeList(recipe);
+            recipeList.appendChild(recipeItem);
+        });
+    } 
+}
+
+
+
+// Functions to select and display My Menu
+
 function selectRecipe(recipe) {
     if (!selectedRecipes.includes(recipe)) {
         selectedRecipes.push(recipe);
@@ -55,15 +82,16 @@ function selectRecipe(recipe) {
     }
 }
 
-// Function to display selected recipes
 function displaySelectedRecipes() {
     selectedRecipeList.innerHTML = '';
     selectedRecipes.forEach((recipe) => {
-        const selectedRecipeItem = createRecipeListItem(recipe);
+        const selectedRecipeItem = createRecipeList(recipe);
         selectedRecipeList.appendChild(selectedRecipeItem);
     });
 }
-// Function to fetch and display ingredients for a selected recipe
+
+// Function to fetch and display ingredients for each recipe
+
 async function displayIngredients(recipe) {
     try {
         const data = await fetchData(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipe.idMeal}`);
@@ -74,42 +102,41 @@ async function displayIngredients(recipe) {
     }
 }
 
-// Function to extract ingredients from a recipe object
+// Function to extract the ingredients from the recipe object
+
 function getIngredientsList(recipe) {
-    const ingredients = [];
+    const ingredients = []; // Ingredients array
     for (let i = 1; i <= 20; i++) {
         const ingredient = recipe[`strIngredient${i}`];
         const measure = recipe[`strMeasure${i}`];
         if (ingredient && ingredient.trim() !== '') {
             ingredients.push(`${measure} ${ingredient}`);
         } else {
-            break; // No more ingredients
+            break; 
         }
     }
     return ingredients;
 }
 
-// Function to display ingredients in the ingredient list
+// Functions to create and display the list of ingredients per recipe
+
+function createIngredientList(ingredient) {
+    const ingredientItem = document.createElement('li');
+    ingredientItem.textContent = ingredient;
+    ingredientItem.addEventListener('click', () => {
+        addToShoppingList(ingredient); // Adds Ingredient to Shopping list
+    });
+    return ingredientItem;
+}
+
 function displayIngredientsList(ingredients) {
     ingredientList.innerHTML = '';
     if (ingredients.length > 0) {
         ingredients.forEach((ingredient) => {
-            const ingredientItem = createIngredientListItem(ingredient);
+            const ingredientItem = createIngredientList(ingredient);
             ingredientList.appendChild(ingredientItem);
         });
-    } else {
-        ingredientList.textContent = 'No ingredients found.';
-    }
-}
-
-// Function to create a list item for an ingredient
-function createIngredientListItem(ingredient) {
-    const ingredientItem = document.createElement('li');
-    ingredientItem.textContent = ingredient;
-    ingredientItem.addEventListener('click', () => {
-        addToShoppingList(ingredient);
-    });
-    return ingredientItem;
+    } 
 }
 
 // Function to add selected ingredients to the shopping list
@@ -130,21 +157,3 @@ function displayShoppingList() {
     });
 }
 
-// Event listener for the search button
-document.getElementById('searchButton').addEventListener('click', () => {
-    const query = searchInput.value.trim();
-    if (query !== '') {
-        fetchRecipes(query);
-    }
-});
-
-// Function to display the shopping list
-function displayShoppingList() {
-    shoppingList.innerHTML = '';
-    shoppingListItems.forEach((item) => {
-        const shoppingListItem = document.createElement('li');
-        shoppingListItem.textContent = item;
-        shoppingList.appendChild(shoppingListItem);
-    });
-
-}
